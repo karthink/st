0445fcf761f5de2a4b381c16171c3db9da078541
Karthik Chikmagalur <karthikchikmagalur@gmail.com>
2020-06-03 14:31:40 -0700

    Changed default font to Iosevka (from Iosevka Nerd Font)

--- config.h
+++ config.h
@@ -5,10 +5,12 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Iosevka Nerd Font Mono:pixelsize=16:antialias=true:autohint=true";
+static char *font = "Iosevka:pixelsize=18:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
   "Fantasque Sans Mono:pixelsize=18:antialias=true:autohint=true",
+  "Iosevka Nerd Font Mono:pixelsize=16:antialias=true:autohint=true",
+  "Monospace:pixelsize=18:antialias=true:autohint=true",
 /*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
 /*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
 };
