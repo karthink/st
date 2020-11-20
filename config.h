f0b96fc4322591996ba377ac946c248e256516f4
Karthik Chikmagalur <karthikchikmagalur@gmail.com>
2020-11-19 23:52:41 -0800

    Reduced font size of Ubuntu Mono

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Ubuntu Mono:pixelsize=19:antialias=true:autohint=true";
+static char *font = "Ubuntu Mono:pixelsize=18:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
   "Iosevka:pixelsize=16:antialias=true:autohint=true",
